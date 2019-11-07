3abf45552cc3cfc6980b596130bca5c8742cd3bc
Rodrigo Venâncio Veríssimo <rodrigovenancioverissimo@gmail.com>
2019-11-07 11:54:12 -0400

    Atualizado para Boostrap 4 e iniciado modelo da Home

---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import {HomeComponent} from './home/home.component'
+import {OfertaComponent} from './oferta/oferta.component'
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
+
+export const ROUTES: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'oferta', component: OfertaComponent}
+]
---
+++ src/app/oferta/oferta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bc-oferta',
+  templateUrl: './oferta.component.html',
+  styleUrls: ['./oferta.component.scss']
+})
+export class OfertaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
